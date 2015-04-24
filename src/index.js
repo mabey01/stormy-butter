72fe8fe7c3c419ab56368684ba6e5e223c47a541
Maximilian <mabey01@gmail.com>
2015-04-25 00:14:14 +0200

    first contact

--- src/index.js
+++ src/index.js
@@ -2,4 +2,18 @@
  * Created by Maximilian on 25.04.2015.
  */
 
+var express = require('express');
+var app = express();
 
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+var server = app.listen(4444, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+
+});
